574d35c34ac8bd5ceae367f059784b680bc58ecb
shusaku009 <shusaku009@gmail.com>
2023-11-15 17:38:16 +0900

    add: コールバック関数の挙動について

--- 03_js_basic/040_callback/start/main.js
+++ 03_js_basic/040_callback/start/main.js
@@ -1,15 +1,13 @@
-function fn(number) {
-    return number * 2;
-  }
-  
-  console.log(fn(2));
-  console.log(fn(4));
-  
-  const fnArrow = number => {
-    console.log(number);
-    return number * 2;
-  };
-  
-  const fnArrowObj = number => ({ result: number * 2 })
-  
-  console.log(fnArrowObj(2));
+function print(callback) {
+  console.log(callback);
+  const result = callback(2);
+  console.log(result);
+}
+
+
+function fn(number = 3) {
+  return number * 2;
+}
+
+debugger;
+print(fn);
