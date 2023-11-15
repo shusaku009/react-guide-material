acfbb53ee722b2656470721001f35976567f6c07
shusaku009 <shusaku009@gmail.com>
2023-11-15 16:59:52 +0900

    add: アロー関数の記法

--- 03_js_basic/020_arrow_function/start/main.js
+++ 03_js_basic/020_arrow_function/start/main.js
@@ -1,3 +1,22 @@
 function fn(number) {
   return number * 2;
 }
+
+console.log(fn(2));
+
+// const fnArrow = (number) => {
+//   return number * 2;
+// }
+
+// アロー関数の省略形
+// const fnArrow = number => number * 2;
+
+const fnArrow = number => {
+  console.log(number);
+  return number * 2
+};
+console.log(fnArrow(2));
+
+const fnArrowObj = number => ({ result: number * 2 });
+
+console.log(fnArrowObj(2));
