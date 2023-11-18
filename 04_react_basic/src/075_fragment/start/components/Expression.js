a03a752275fbcedbb4b814e73087d10bdc294d9a
shusaku009 <shusaku009@gmail.com>
2023-11-18 14:10:23 +0900

    add: JSX内でJSコードを実行してみよう

---
+++ 04_react_basic/src/075_fragment/start/components/Expression.js
@@ -0,0 +1,18 @@
+import "./Expression.css";
+
+const Expression = () => {
+  const title = "Expression";
+  const arry = ['item1', 'item2', 'item3'];
+  const hello = (arg) => `${arg} Function`;
+  return (
+    <div className={title.toLowerCase()}>
+      <h3>Hello {title}</h3>
+      <h3>{arry}</h3>
+      <h3>{hello('Hello')}</h3>
+      <h3>{/* 画面上に表示されません */}</h3>
+      {<h3>Hello JSX</h3>}
+    </div>
+  );
+};
+
+export default Expression;
