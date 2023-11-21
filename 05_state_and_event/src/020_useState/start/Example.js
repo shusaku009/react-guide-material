a2a18bc2d42c71b6374e0cfa2ac015528079c0d5
shusaku009 <shusaku009@gmail.com>
2023-11-21 23:42:37 +0900

    add: useStateについて

--- 05_state_and_event/src/020_useState/start/Example.js
+++ 05_state_and_event/src/020_useState/start/Example.js
@@ -1,8 +1,15 @@
-const Example = () => {
+import { useState } from "react";
 
+const Example = () => {
+  let [val, setVal] = useState(0);
   return (
     <>
-      
+      <input 
+        type="text"
+        onChange={(e) => {
+          // const setFn = val[1];
+          setVal(e.target.value)
+        }} /> = {val}
     </>
   );
 };
