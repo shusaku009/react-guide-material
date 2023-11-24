755ffa09b2dc1edf5f150ac4f0e3bdc33557264d
shusaku009 <shusaku009@gmail.com>
2023-11-25 06:50:27 +0900

    add: 配列をリスト表示

--- 06_control_and_form/src/010_list_components/start/Example.js
+++ 06_control_and_form/src/010_list_components/start/Example.js
@@ -3,11 +3,21 @@ const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
 
+  // 配列処理でfor文はあまり使用しない
+  // const animalList = [];
+  // for(const animal of animals ){
+  //   animalList.push(<li>{animal}</li>)
+  // }
+
+  // const helloAnimals = animals.map((animal) => <li>Hello, {animal}</li>)
+
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        
+        {/* {animalList} */}
+        {/* {helloAnimals} */}
+        {animals.map((animal) => <li>Hello, {animal}</li>)}
       </ul>
     </>
   );
