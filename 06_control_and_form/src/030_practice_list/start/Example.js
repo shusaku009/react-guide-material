fb6b1242bd225753c2db1868a5ada2afcb53b13b
shusaku009 <shusaku009@gmail.com>
2023-11-25 13:57:33 +0900

    add: 練習問題_リストにキーを設定

--- 06_control_and_form/src/030_practice_list/start/Example.js
+++ 06_control_and_form/src/030_practice_list/start/Example.js
@@ -1,3 +1,20 @@
+import Profile  from './components/Profile';
+
+const profiles = [
+  {name: "Geo", 
+  age: 18, 
+  hobbies: ["sports", "music"],
+  },
+  {name: "Tom", 
+  age: 25, 
+  hobbies: ["movie", "music"],
+  },
+  {name: "Geo", 
+  age: 18, 
+  hobbies: ["sports", "travel", "game"],
+  },
+];
+
 const Example = () => {
   return (
     <>
@@ -5,7 +22,11 @@ const Example = () => {
       <p>Profileコンポーネントを使用して、完成コードと同じ画面を作成してください。</p>
       <p>また、Profileコンポーネント内のリスト表示部分にkeyを設定して、ワーニング表示がされないようにしてください。</p>
       <ul>
-        {/* ここに記述 */}
+        {profiles.map((profile) => (
+          <li key={profile.name}>
+            <Profile {...profile} />
+          </li>
+        ))}
       </ul>
     </>
   );
