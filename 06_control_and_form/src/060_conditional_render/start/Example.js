f0d30102096347ba2de9bfc7fb7ca0c1f27f640e
shusaku009 <shusaku009@gmail.com>
2023-11-25 15:43:12 +0900

    add: 条件分岐について

--- 06_control_and_form/src/060_conditional_render/start/Example.js
+++ 06_control_and_form/src/060_conditional_render/start/Example.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const Example = () => {
-  const animals = ["Dog", "Cat", "Rat"];
+  const animals = ["Dog", "Cat", null, "Rat"];
 
   const [filterVal, setFilterVal] = useState("");
 
@@ -15,13 +15,23 @@ const Example = () => {
       <ul>
         {animals
           .filter((animal) => {
-            const isMatch = animal.indexOf(filterVal) !== -1;
-            console.log(animal.indexOf(filterVal));
+            const animalStr = animal ?? "";
+            const isMatch = animalStr.indexOf(animalStr) !== -1;
+            console.log(animalStr.indexOf(filterVal));
             return isMatch;
           })
-          .map((animal) => (
-            <li key={animal}>{animal}</li>
-          ))}
+          .map((animal) => {
+            return <li key={animal}>{
+              animal ?? "null,undefinedでした"
+            }{animal === "Dog" && "★"}</li>;
+            // ３項演算子
+            // return <li key={animal}>{animal === "Dog" ? animal + "★" : animal}</li>
+            // if(animal === "Dog") {
+            //   return <li key={animal}>{animal}★</li>
+            // } else {
+            //   return <li key={animal}>{animal}</li>
+            // }
+          })}
       </ul>
     </>
   );
