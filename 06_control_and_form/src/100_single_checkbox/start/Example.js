fe294c7f4de696ff83b6ffcd3b39f424c0d2ed54
shusaku009 <shusaku009@gmail.com>
2023-11-27 00:20:34 +0900

    add: チェックボックスの作成方法

--- 06_control_and_form/src/100_single_checkbox/start/Example.js
+++ 06_control_and_form/src/100_single_checkbox/start/Example.js
@@ -1,10 +1,30 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [isChecked, setIsChecked] = useState(true);
+
+  const toggleChecked = (e) => {
+    setIsChecked(preveState => !preveState);
+    // ↑下のコードの省略形
+    // setIsChecked((prevState) => {
+    //   let state = !prevState;
+    //   return state;
+    // });
+  };
+
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <div>
+      <label htmlFor="my-check">
+        チェック : 
+      </label>
+      <input
+        type="checkbox"
+        id="my-check"
+        checked={isChecked}
+        onChange={(e) => toggleChecked(e)}
+      />
+      <div>{isChecked ? "ON!" : "OFF!"}</div>
+    </div>
   );
 };
 
