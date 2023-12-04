576795e55d1d657b59c31cdd19c1576ddb6a178b
shusaku009 <shusaku009@gmail.com>
2023-12-04 23:57:17 +0900

    add: ChakraUIを使ってみよう

--- 07_styling_component/src/050_chakra_ui/start/components/List.js
+++ 07_styling_component/src/050_chakra_ui/start/components/List.js
@@ -1,18 +1,30 @@
+import { StackDivider, VStack } from "@chakra-ui/react";
+
 const List = ({todos, deleteTodo}) => {
     const complete = (id) => {
         deleteTodo(id)
     }
     return (
-        <div>
-            {todos.map(todo => {
-                return (
-                    <div key={todo.id}>
-                        <button onClick={() => complete(todo.id)}>完了</button>
-                        <span>{todo.content}</span>
-                    </div>
-                )
-            })}
-        </div>
+        <VStack
+					divider={<StackDivider/>}
+					// color={{ sm: `red.600`, md: `blue.600`, lg: `green.500`, xl: `red.600`}}
+					borderColor="black.100"
+					borderWidth="1px"
+					borderRadius="3px" // border-radius: 3px
+					p={5}
+					alignItems="start"
+        >
+            <div>
+                {todos.map(todo => {
+                    return (
+                        <div key={todo.id}>
+                            <button onClick={() => complete(todo.id)}>完了</button>
+                            <span>{todo.content}</span>
+                        </div>
+                    )
+                })}
+            </div>
+        </VStack>
     );
 }
 
