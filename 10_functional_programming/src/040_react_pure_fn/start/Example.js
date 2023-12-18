4c1e55dc41c7d74e0d50445195069669778cd3d3
shusaku009 <shusaku009@gmail.com>
2023-12-18 23:16:42 +0900

    add: Reactto

--- 10_functional_programming/src/040_react_pure_fn/start/Example.js
+++ 10_functional_programming/src/040_react_pure_fn/start/Example.js
@@ -1,7 +1,24 @@
-const Example = () => {
+let value = 0;
 
+const Child = () => {
+  value++;
+  return <div>{value}</div>
+}
+
+const ChildPure = ({ value }) => {
+  return <div>{value}</div>
+}
+
+const Example = () => {
+  let value = 0;
   return (
     <>
+      <Child />
+      <Child />
+      <Child />
+      <ChildPure value={++value}/>
+      <ChildPure value={++value}/>
+      <ChildPure value={++value}/>
     </>
   );
 };
