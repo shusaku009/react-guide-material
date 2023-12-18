c331d1ac50828a4082784bca6eef5a786df0720c
shusaku009 <shusaku009@gmail.com>
2023-12-18 20:40:40 +0900

    add: 不変性

--- 10_functional_programming/src/030_immutability/start/Example.js
+++ 10_functional_programming/src/030_immutability/start/Example.js
@@ -1,4 +1,17 @@
 const Example = () => {
+
+  const num = { val: 2 };
+
+  const double = (num) => {
+    const newNum = {};
+    
+    num.val = num.val * 2;
+    return num;
+  }
+
+  const newNum = double(num);
+  console.log('newNum', newNum, 'num', num);
+  console.log(newNum === num);
   return (
     <>
       <h3>不変性（Immutability）</h3>
