6cbb4d4c2a1fa04813ad961f3a6c5aea236e4c3d
shusaku009 <shusaku009@gmail.com>
2023-12-23 10:47:50 +0900

    clean useContextのリファクタリング

--- 11_hooks_p1/src/050_context_file/start/components/Header.js
+++ 11_hooks_p1/src/050_context_file/start/components/Header.js
@@ -1,5 +1,23 @@
+import { useTheme } from "../context/ThemeContext";
+
 const Header = () => {
-  
+  const [theme, setTheme] = useTheme();
+  const THEMES = ['light', 'dark', 'red'];
+
+  const changeTheme = (e) => setTheme(e.target.value)
+
+  return (
+    <header className={`content-${theme}`}>
+        {THEMES.map(_theme => {
+          return (
+            <label>
+              <input type="radio" key={_theme} value={_theme} checked={theme === _theme} onChange={changeTheme} />
+              {_theme}
+            </label>
+          )
+        })};
+      </header>
+  )
 };
 
 export default Header;
