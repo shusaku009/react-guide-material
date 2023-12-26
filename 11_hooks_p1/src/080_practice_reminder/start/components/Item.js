f0693ca6e1e49c0653df495a01e6e8e2c0cf97fa
shusaku009 <shusaku009@gmail.com>
2023-12-26 09:37:21 +0900

    add: 練習問題_useContextとuseReducer

--- 11_hooks_p1/src/080_practice_reminder/start/components/Item.js
+++ 11_hooks_p1/src/080_practice_reminder/start/components/Item.js
@@ -1,24 +1,31 @@
 import { useState } from "react";
+import { useDispatchTodos } from "../context/TodoContext";
 
-const Item = ({todo, complete, updateTodo}) => {
+const Item = ({todo}) => {
   const [ editingContent, setEditingContent ] = useState(todo.content)
+
+  const dispatch = useDispatchTodos();
 
   const changeContent = (e) => setEditingContent(e.target.value);
 
   const toggleEditMode = () => {
     const newTodo = { ...todo, editing: !todo.editing };
-    updateTodo(newTodo)
+    dispatch({type: 'todo/update', todo: newTodo })
   };
 
   const confirmContent = (e) => {
     e.preventDefault();
     const newTodo = { ...todo, editing: !todo.editing, content: editingContent };
-    updateTodo(newTodo)
+    dispatch({type: 'todo/update', todo: newTodo })
   };
+
+  const complete = (todo) => {
+    dispatch({ type: 'todo/delete', todo })
+  }
 
   return (
     <div key={todo.id}>
-        <button onClick={() => complete(todo.id)}>完了</button>
+        <button onClick={() => complete(todo)}>完了</button>
         <form onSubmit={confirmContent} style={{ display: "inline"}}>
           {
             todo.editing ? (
