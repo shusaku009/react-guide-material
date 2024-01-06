065d29cf3795c902526767fe4f8fd45462ac72ed
shusaku009 <shusaku009@gmail.com>
2024-01-06 13:59:24 +0900

    add: Redux Toolkitにおけるミューダブルな値の変更

--- 13_redux/start/src/040_immer/components/CounterResult.js
+++ 13_redux/start/src/040_immer/components/CounterResult.js
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux"
 const CounterResult = () => {
-  const state = useSelector(state => state);
-  return <h3>{state.counter}</h3>;
+  const count = useSelector(state => state.counter);
+  return <h3>{count.counter}</h3>;
 };
 
 export default CounterResult;
--- 13_redux/start/src/040_immer/store/modules/counter.js
+++ 13_redux/start/src/040_immer/store/modules/counter.js
@@ -2,15 +2,22 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const counter = createSlice({
   name: 'counter',
-  initialState: 0,
+  initialState: {
+    count: 0
+  },
   reducers: {
     add(state, { type, payload }) {
+      // state.count = state.count + payload
       console.log(type, payload)
-      return state + payload;
+      const newState = { ...state };
+      newState.count = state.count + payload
+      return newState;
     },
     minus(state, { type, payload }) {
       console.log(type, payload)
-      return state - payload;
+      const newState = { ...state };
+      newState.count = state.count - payload
+      return newState;
     }
   }
 })
