93a4c02991f82b5ae6dc6d7a10e2a9113949efae
shusaku009 <shusaku009@gmail.com>
2024-01-07 16:36:07 +0900

    add: Immerを使ったミュータブルな値の変更

---
+++ 13_redux/start/src/040_immer/immer.js
@@ -0,0 +1,17 @@
+import { produce } from "immer";
+
+const state = {
+  name: "Tom",
+  hobbies: ["tennis", "soccer"]
+}
+
+// const newState = state;
+// newState.name = "John";
+const newState = produce(state, draft => {
+  draft.name = "John";
+  draft.hobbies[0] = "baseball";
+  console.log(draft);
+  // return state;
+})
+
+console.log(state, newState);
