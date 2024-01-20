89bcd07246f9536e544efd14ec77849e600ba827
shusaku009 <shusaku009@gmail.com>
2024-01-20 15:51:41 +0900

    Add: 取得したデータを画面に表示する

--- 16_rest_api/src/040_axios_get_request/start/Example.js
+++ 16_rest_api/src/040_axios_get_request/start/Example.js
@@ -1,13 +1,15 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
 import axios from "axios";
 
+// axiosでリクエストを送信
 const Example = () => {
   useEffect(() => {
     const getUser = async () => {
-      const res = await axios.get('http://localhost:3001')
-      console.log(res)
+      const res = await axios.get('http://localhost:3003/user')
+      console.log(res.data)
     }
-  });
+    getUser();
+  })
 };
 
 export default Example;
